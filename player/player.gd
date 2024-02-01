f51e18dc949bd9ab8f903611aa7c805d95f814e4
Steve <stshryu@gmail.com>
2024-01-31 22:03:38 -0500

    Adding collision and health to player and enemies

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
-@onready var animations = $AnimationPlayer
+class_name Player
 
+signal healthChanged
+
+@export var speed: int = 60
+@onready var animations = $AnimationPlayer
+@onready var effects = $Effects
+@onready var hurtTimer = $hurtTimer
+
+@export var maxHealth = 3
+@onready var currentHealth: int = maxHealth
+
+@export var knockbackPower: int = 500
+
+func _ready():
+	effects.play("RESET")
+	
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
@@ -19,7 +33,30 @@ func updateAnimation():
 
 		animations.play("walk" + direction)
 
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	handleCollision()
 	updateAnimation()
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "hitBox":
+		currentHealth -= 1
+		if currentHealth < 0:
+			currentHealth = maxHealth
+		healthChanged.emit(currentHealth)
+		knockback(area.get_parent().velocity)
+		effects.play("hurtBlink")
+		hurtTimer.start()
+		await hurtTimer.timeout
+		effects.play("RESET")
+
+func knockback(enemyVelocity):
+	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
