fd9c4804c8c42f5854742e1d366edcfbe6dc074b
Steve <stshryu@gmail.com>
2024-02-03 18:45:10 -0500

    Tutorial Done, starting actual project

--- player/player.gd
+++ player/player.gd
@@ -1,62 +1,48 @@
 extends CharacterBody2D
 
-class_name Player
+@export var walk_speed = 4.0
+const TILE_SIZE = 16
 
-signal healthChanged
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
 
-@export var speed: int = 60
-@onready var animations = $AnimationPlayer
-@onready var effects = $Effects
-@onready var hurtTimer = $hurtTimer
+var initial_position = Vector2(0, 0)
+var input_direction = Vector2(0, 0)
+var is_moving = false
+var percent_to_next_tile = 0.0
 
-@export var maxHealth = 3
-@onready var currentHealth: int = maxHealth
+func process_player_input():
+	if input_direction.y == 0:
+		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	if input_direction.x == 0:
+		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 
-@export var knockbackPower: int = 500
+	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Walk/blend_position", input_direction)
+		initial_position = position
+		is_moving = true
+	else:
+		anim_state.travel("Idle")
+
+func move(delta):
+	percent_to_next_tile += walk_speed * delta
+	if percent_to_next_tile >= 1.0:
+		position = initial_position + (TILE_SIZE * input_direction)
+		percent_to_next_tile = 0.0
+		is_moving = false
+	else:
+		position = initial_position + (TILE_SIZE * input_direction * percent_to_next_tile)
 
 func _ready():
-	effects.play("RESET")
-	
-func handleInput():
-	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection*speed
-
-func updateAnimation():
-	if velocity.length() == 0:
-		if animations.is_playing():
-			animations.stop()
-	else:
-		var direction = "Down"
-		if velocity.x < 0: direction = "Left"
-		elif velocity.x > 0: direction = "Right"
-		elif velocity.y < 0: direction = "Up"
-
-		animations.play("walk" + direction)
-
-func handleCollision():
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
+	initial_position = position
 
 func _physics_process(delta):
-	handleInput()
-	move_and_slide()
-	handleCollision()
-	updateAnimation()
-
-func _on_hurt_box_area_entered(area):
-	if area.name == "hitBox":
-		currentHealth -= 1
-		if currentHealth < 0:
-			currentHealth = maxHealth
-		healthChanged.emit(currentHealth)
-		knockback(area.get_parent().velocity)
-		effects.play("hurtBlink")
-		hurtTimer.start()
-		await hurtTimer.timeout
-		effects.play("RESET")
-
-func knockback(enemyVelocity):
-	var knockbackDirection = (enemyVelocity - velocity).normalized() * knockbackPower
-	velocity = knockbackDirection
-	move_and_slide()
+	if is_moving == false:
+		process_player_input()
+	elif input_direction != Vector2.ZERO:
+		anim_state.travel("Walk")
+		move(delta)
+	else:
+		anim_state.travel("Idle")
+		is_moving = false
