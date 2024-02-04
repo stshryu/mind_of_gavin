63932aa98d88b72a1aaa5e4b93ab3820e4564862
Steve <stshryu@gmail.com>
2024-02-04 02:48:59 -0500

    testing some player movement options

--- player/player.gd
+++ player/player.gd
@@ -1,48 +1,71 @@
 extends CharacterBody2D
 
-@export var walk_speed = 4.0
-const TILE_SIZE = 16
+@onready var ray_cast: RayCast2D = $CollisionRaycast
+@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var anim_tree_properties = $AnimationTree.get("parameters/playback")
 
-@onready var anim_tree = $AnimationTree
-@onready var anim_state = anim_tree.get("parameters/playback")
+const TILE_SIZE: int = 16
+@export var walk_speed: int = 6
 
-var initial_position = Vector2(0, 0)
-var input_direction = Vector2(0, 0)
-var is_moving = false
-var percent_to_next_tile = 0.0
+var init_pos: Vector2 = Vector2.ZERO
+var input_dir: Vector2 = Vector2.ZERO
 
-func process_player_input():
-	if input_direction.y == 0:
-		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	if input_direction.x == 0:
-		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+var is_moving: bool = false
+var is_running: bool = false
+var percent_moved: float = 0.0 # Amount moved to next tile
 
-	if input_direction != Vector2.ZERO:
-		anim_tree.set("parameters/Idle/blend_position", input_direction)
-		anim_tree.set("parameters/Walk/blend_position", input_direction)
-		initial_position = position
-		is_moving = true
-	else:
-		anim_state.travel("Idle")
+var next_step: Vector2
 
-func move(delta):
-	percent_to_next_tile += walk_speed * delta
-	if percent_to_next_tile >= 1.0:
-		position = initial_position + (TILE_SIZE * input_direction)
-		percent_to_next_tile = 0.0
+func _ready() -> void:
+	anim_tree.active = true
+	anim_tree_properties.travel("Idle")
+
+func player_input() -> void:
+	if input_dir.y == 0:
+		input_dir.x = Input.get_axis("ui_left", "ui_right")
+	if input_dir.x == 0:
+		input_dir.y = Input.get_axis("ui_up", "ui_down")
+
+	next_step = input_dir * TILE_SIZE
+
+	ray_cast.target_position = next_step
+	ray_cast.force_raycast_update()
+
+	if Input.is_action_pressed("ui_select"):
+		is_running = true
+		walk_speed = 12
+
+	if not ray_cast.is_colliding():
+		if input_dir != Vector2.ZERO:
+			init_pos = position
+			is_moving = true
+
+func move(delta: float) -> void:
+	percent_moved += walk_speed * delta
+
+	if percent_moved >= 1.0:
+		position = init_pos + next_step
+		percent_moved = 0.0
 		is_moving = false
 	else:
-		position = initial_position + (TILE_SIZE * input_direction * percent_to_next_tile)
+		position = init_pos + (next_step * percent_moved)
 
-func _ready():
-	initial_position = position
+func animation() -> void:
+	if input_dir != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_dir)
+		anim_tree.set("paremeters/Walk/blend_position", input_dir)
 
-func _physics_process(delta):
-	if is_moving == false:
-		process_player_input()
-	elif input_direction != Vector2.ZERO:
-		anim_state.travel("Walk")
+		if is_running:
+			anim_tree_properties.travel("Walk")
+		else:
+			anim_tree_properties.travel("Walk")
+	else:
+		anim_tree_properties.travel("Idle")
+
+func _physics_process(delta: float) -> void:
+	if not is_moving:
+		anim_tree_properties.travel("Idle")
+		player_input()
+	elif is_moving:
 		move(delta)
-	else:
-		anim_state.travel("Idle")
-		is_moving = false
+		animation()
