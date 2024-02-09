1ca938787e465d3cc58d67151257c299da5ced69
Steve <stshryu@gmail.com>
2024-02-09 13:47:35 -0500

    Added shaders and noise to water effects

--- scenes/OverworldTileMap.gd
+++ scenes/OverworldTileMap.gd
@@ -2,8 +2,7 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug("hello")
-	print_debug(get_layers_count())
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
