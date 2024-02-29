acdfd4f7d7b50327b153b35a30f1de98fa94e322
Steve <stshryu@gmail.com>
2024-02-28 21:13:00 -0500

    Adding bag menu options carousel

--- scenes/ui/bag_screen.gd
+++ scenes/ui/bag_screen.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 @onready var select_point = $Control/SelectPoint
-@onready var scene_parent = $"."
+@onready var bag_option_root = $Control/OptionsContainer
+@onready var scene_root = $"."
 
 @export var init_x = 43
 @export var offset_x = 8
 
 enum BAGSCREEN { KEY, ITEM, TMHM, BERRIES, BALLS }
-var current_screen = BAGSCREEN.KEY
-var selected_option: int = 0
+var selected_option: int
 var scene_manager: Node2D
+var bag_options: Array
+var bag_opt_length: int
 
 var is_active_screen = false
 
@@ -17,23 +19,37 @@ signal active_main_menu
 
 func _ready():
 	active_main_menu.connect(get_parent().active_main_menu)
-	scene_parent.visible = false
+	scene_root.visible = false
 	scene_manager = Utils.get_scene_manager()
-	select_point.position.y = init_x + (selected_option % len(BAGSCREEN)) * offset_x
+	bag_options = bag_option_root.get_children()
+	bag_opt_length = bag_options.size()
+	selected_option = 0
+	set_active_bag_option()
 
 func _on_menu_active_bag_menu():
 	is_active_screen = true
-	scene_parent.visible = true
+	scene_root.visible = true
 
 func open_main_menu():
 	is_active_screen = false
 	active_main_menu.emit()
-	scene_parent.visible = false
+	scene_root.visible = false
 
 func _unhandled_input(event):
 	if !is_active_screen:
 		return
-	print('bag is active')
 	if event.is_action_pressed("back"):
 		open_main_menu()
-			
+	elif event.is_action_pressed("ui_left"):
+		selected_option = bag_opt_length - 1 if selected_option == 0 else selected_option - 1
+		set_active_bag_option()
+	elif event.is_action_pressed("ui_right"):
+		selected_option += 1
+		set_active_bag_option()
+
+func set_active_bag_option():
+	var active_option = bag_options[selected_option % bag_opt_length]
+	select_point.position.x = init_x + (selected_option % bag_opt_length) * offset_x
+
+	for option in bag_options:
+		option.visible = true if option.name == active_option.name else false
--- scenes/ui/party_screen.gd
+++ scenes/ui/party_screen.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var scene_parent = $"."
+@onready var scene_root = $"."
 
 var scene_manager: Node2D
 
@@ -11,22 +11,21 @@ signal active_main_menu
 
 func _ready():
 	active_main_menu.connect(get_parent().active_main_menu)
-	scene_parent.visible = false
+	scene_root.visible = false
 	scene_manager = Utils.get_scene_manager()
 
 func _on_menu_active_party_menu():
 	is_active_screen = true
-	scene_parent.visible = true
+	scene_root.visible = true
 	
 func open_main_menu():
 	is_active_screen = false
 	active_main_menu.emit()
-	scene_parent.visible = false
+	scene_root.visible = false
 
 func _unhandled_input(event):
 	if !is_active_screen:
 		return
-	print('party is active')
 	if event.is_action_pressed("back"):
 		open_main_menu()
 			
