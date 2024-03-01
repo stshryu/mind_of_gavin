b2dbdc974f29955e58c1e0fdb614fde2013d41d8
Steve <stshryu@gmail.com>
2024-02-29 19:33:29 -0500

    adding bag animations

--- scenes/ui/bag_screen.gd
+++ scenes/ui/bag_screen.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var select_point = $Control/SelectPoint
 @onready var bag_option_root = $Control/OptionsContainer
+@onready var animation_player = $Control/AnimationPlayer
 @onready var scene_root = $"."
 
 @export var init_x = 43
@@ -50,6 +51,40 @@ func _unhandled_input(event):
 func set_active_bag_option():
 	var active_option = bag_options[selected_option % bag_opt_length]
 	select_point.position.x = init_x + (selected_option % bag_opt_length) * offset_x
-
 	for option in bag_options:
 		option.visible = true if option.name == active_option.name else false
+	animation_player.play(active_option.name)
+	
+# This bit of code below is "supposed" to create an animation through code so you don't have to animate
+# each individual bag sprite. Can't get it to work though, the documentation surrounding the AnimationPlayer
+# is too vague to really piece together what you need to write animations through code. Maybe its something
+# to revisit later when we have time to really explore the engine code.
+
+#func _create_bag_shake():
+	#var anim_lib = AnimationLibrary.new()
+	#for option in bag_options:
+		#var animation = Animation.new()
+		#animation.step = 0.1
+		#animation.length = 0.2
+#
+		#var texture = option.find_children("Bag").back()
+		#var sprite_path: NodePath = scene_root.get_path_to(texture)
+		#
+		#var sprite_vis_path: NodePath = "%s:visible" % sprite_path
+		#var vis_track_index = animation.add_track(Animation.TYPE_VALUE)
+		#animation.track_set_path(vis_track_index, sprite_vis_path)
+		#animation.track_insert_key(vis_track_index, 0.0, true)
+		#
+		#var sprite_texture_path: NodePath = "%s:texture" % sprite_path
+		#var texture_track_index = animation.add_track(Animation.TYPE_VALUE)
+		#animation.track_set_path(texture_track_index, sprite_texture_path)
+		#animation.track_insert_key(texture_track_index, 0.0, texture)
+		#
+		#var sprite_rotation_path: NodePath = "%s:rotation" % sprite_path
+		#var rota_track_index = animation.add_track(Animation.TYPE_VALUE)
+		#animation.track_set_path(rota_track_index, sprite_rotation_path)
+		#animation.track_insert_key(rota_track_index, 0.0, 4)
+		#animation.track_insert_key(rota_track_index, 0.2, 0)
+		#
+		#anim_lib.add_animation(option.name, animation)
+	#animation_player.add_animation_library("bag_anim", anim_lib)
